806e3eeb046f3bcec6a92db3d04af3e64463d124
alpha dev <alpha5611331@gmail.com>
2026-04-14 20:15:50 +0200

    implemented - show own model if provided by user

--- src/renderer/pages/main/index.tsx
+++ src/renderer/pages/main/index.tsx
@@ -255,7 +255,7 @@ export default function MainPage() {
         <StatusPanel
           runningState={appState?.runningState ?? RunningState.Idle}
           credits={appState?.credits ?? 0}
-          llmModel={appState?.providedLLMModel ?? ''}
+          llmModel={config?.llmConf?.model ?? appState?.providedLLMModel ?? ''}
         />
       )}
 
